cc2907353b1b9e8575f18c8219fe4a3df69550dc
IMLmrchen <wxlxdxrx1456s@outlook.com>
2026-05-02 18:57:53 +0800

    Update password for download function

--- api/download.js
+++ api/download.js
@@ -1,7 +1,7 @@
 // Vercel Serverless Function for password-protected downloads
 module.exports = (req, res) => {
   const { file, password } = req.query;
-  const correctPassword = 'cloudisk';
+  const correctPassword = 'chy1216tk91';
 
   if (!password) {
     res.status(401).send('未提供密码');
